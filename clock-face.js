504d01df00140c80eef36ef0477b599e49f213c4
Daryl Metzler <djmetzle@ncsu.edu>
2017-10-12 22:57:18 -0700

    Abstract Clock View out into a class
    This makes the main clock entrypoint and `draw_current`
    functions much simpler.
    
    Also, make the drawing functions a little more slick. Everything,
    including styles, now scales with the canvas element.
    
    Add some slick drop shadows, and reshape the hands to give the face
    some character.
    
    Still needs some work to add the numerals back.

---
+++ clock-face.js
@@ -0,0 +1,103 @@
+// ClockFace Object
+
+var HOUR_HAND_COLOR = "#000000";
+var MINUTE_HAND_COLOR = "#000000";
+var SECOND_HAND_COLOR = "#FF0000";
+
+function ClockFace(draw_ctx) {
+	this.ctx = draw_ctx;
+}
+
+ClockFace.prototype.clearCtx = function() {
+	this.ctx.globalCompositeOperation = 'source-over';
+	this.ctx.clearRect(0, 0, this.size, this.size); // clear canvas
+}
+
+ClockFace.prototype.setSize = function(size) {
+	this.size = size;
+	this.scaleConsts();
+}
+
+ClockFace.prototype.scaleConsts = function() {
+	this.centerPos = this.size / 2.0;
+
+	this.faceRadius = this.centerPos*0.85;
+	this.edgeRadius = this.centerPos*0.90;
+	this.faceWidth = this.centerPos*0.01;
+
+	this.hourRadius = this.centerPos * 0.45;
+	this.minuteRadius = this.centerPos * 0.6;
+	this.secondRadius = this.centerPos * 0.7;
+
+	this.hourWidth = this.centerPos*0.01;
+	this.minuteWidth = this.centerPos*0.01;
+	this.secondWidth = this.centerPos*0.008;
+
+	this.ctx.shadowColor = '#111';
+	this.ctx.shadowBlur = 10;
+	this.ctx.shadowOffsetX = this.faceWidth;
+	this.ctx.shadowOffsetY = this.faceWidth;
+}
+
+ClockFace.prototype.drawFace = function() {
+	this.ctx.lineWidth=this.faceWidth;
+	this.ctx.strokeStyle="#000";
+	this.ctx.shadowColor = '#111';
+	this.ctx.shadowBlur = 10;
+	this.ctx.shadowOffsetX = this.faceWidth;
+	this.ctx.shadowOffsetY = this.faceWidth;
+	this.ctx.beginPath();
+	this.ctx.arc(this.centerPos,this.centerPos,this.faceRadius, 0, 2.0*Math.PI, false);
+	this.ctx.stroke();
+
+	this.ctx.lineWidth=this.faceWidth / 2.0;
+	this.ctx.strokeStyle="#000000";
+	this.ctx.beginPath();
+	this.ctx.arc(this.centerPos,this.centerPos,this.edgeRadius, 0, 2.0*Math.PI, false);
+	this.ctx.stroke();
+
+	this.ctx.fillStyle='#777';
+	this.ctx.beginPath();
+	this.ctx.arc(this.centerPos,this.centerPos,this.faceWidth * 2.0, 0, 2.0*Math.PI, false);
+	this.ctx.fill();
+}
+
+ClockFace.prototype.drawHands = function(clock) {
+	this.drawHourHand(clock);
+	this.drawMinuteHand(clock);
+	this.drawSecondHand(clock);
+}
+
+ClockFace.prototype.drawHourHand = function(clock) {
+	var hourAngle = clock.hourAngle - Math.PI / 2.0;
+	this.drawHand(hourAngle, this.hourRadius, this.hourWidth, HOUR_HAND_COLOR);
+}
+
+ClockFace.prototype.drawMinuteHand = function(clock) {
+	var minuteAngle = clock.minuteAngle - Math.PI / 2.0;
+	this.drawHand(minuteAngle, this.minuteRadius, this.minuteWidth, MINUTE_HAND_COLOR);
+}
+
+ClockFace.prototype.drawSecondHand = function(clock) {
+	var secondAngle = clock.secondAngle - Math.PI / 2.0;
+	this.drawHand(secondAngle, this.secondRadius, this.secondWidth, SECOND_HAND_COLOR);
+}
+
+ClockFace.prototype.drawHand = function(angle, radius, width, color) {
+	var tip_x = this.centerPos + radius * Math.cos(angle);
+	var tip_y = this.centerPos + radius * Math.sin(angle);
+	var t_x = this.centerPos - radius * 0.25 * Math.cos(angle);
+	var t_y = this.centerPos - radius * 0.25 * Math.sin(angle);
+	var l_x = t_x + width * Math.cos(angle + Math.PI / 2.0);
+	var l_y = t_y + width * Math.sin(angle + Math.PI / 2.0);
+	var r_x = t_x - width * Math.cos(angle + Math.PI / 2.0);
+	var r_y = t_y - width * Math.sin(angle + Math.PI / 2.0);
+	this.ctx.lineWidth=width;
+	this.ctx.fillStyle=color;
+	this.ctx.beginPath();
+	this.ctx.moveTo(tip_x, tip_y);
+	this.ctx.lineTo(l_x, l_y);
+	this.ctx.lineTo(r_x, r_y);
+	this.ctx.fill();		
+}
+
--- clock.js
+++ clock.js
@@ -5,6 +5,8 @@ var DEBUG = true;
 var canvas;
 var ctx;
 
+var clockFace;
+
 // constants
 var boxWidth = 100;
 var textRadius = null;
@@ -12,36 +14,6 @@ var hourRad = Math.PI / 6.0;
 var minuteRad = Math.PI / 30.0;
 var secondRad = Math.PI / 30.0;
 var centerPos = 300;
-
-var hourHandRadius = 100;
-var hourHandWidth = 6.0;
-var hourHandColor = "#000000";
-
-var minuteHandRadius = 150;
-var minuteHandWidth = 5.0;
-var minuteHandColor = "#000000";
-
-var secondHandRadius = 180;
-var secondHandWidth = 2.0;
-var secondHandColor = "#FF0000";
-
-function draw_face() {
-	draw_edge();
-	draw_numerals();
-
-}
-
-function draw_edge() {
-	ctx.lineWidth=4.0;
-	ctx.strokeStyle="#000000";
-	ctx.beginPath();
-	ctx.arc(centerPos,centerPos,centerPos * 0.85, 0, 2.0*Math.PI, false);
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.arc(centerPos,centerPos,5, 0, 2.0*Math.PI, false);
-	ctx.fill();
-}
 
 function draw_numerals() {
 	ctx.lineWidth=2.0;
@@ -60,65 +32,17 @@ function draw_numerals() {
 	}
 }
 
-function draw_time_string(timeStr) {
-	ctx.lineWidth=1.0;
-	ctx.strokeStyle="#000000";
-	ctx.font = '18px serif';
-	ctx.textAlign = 'center';
-	ctx.fillText(timeStr, centerPos, boxWidth-10);
-}
-
-function draw_hand(x,y,thickness, color) {
-	ctx.lineWidth=thickness;
-	ctx.strokeStyle=color;
-	ctx.beginPath();
-	ctx.moveTo(centerPos, centerPos);
-	ctx.lineTo(x, y);
-	ctx.stroke();		
-}
-
-function draw_hands(clock) {
-	drawHour(clock);
-	drawMinute(clock);
-	drawSecond(clock);
-}
-
-function drawHour(clock) {
-	var hourAngle = clock.hourAngle - Math.PI / 2.0;
-	var x = centerPos + hourHandRadius * Math.cos(hourAngle);
-	var y = centerPos + hourHandRadius * Math.sin(hourAngle);
-	draw_hand(x,y,hourHandWidth, hourHandColor);
-}
-
-function drawMinute(clock) {
-	var minuteAngle = clock.minuteAngle - Math.PI / 2.0;
-	var x = centerPos + minuteHandRadius * Math.cos(minuteAngle);
-	var y = centerPos + minuteHandRadius * Math.sin(minuteAngle);
-	draw_hand(x,y,minuteHandWidth, minuteHandColor);
-}
-
-function drawSecond(clock) {
-	var secondAngle = clock.secondAngle - Math.PI / 2.0;
-	var x = centerPos + secondHandRadius * Math.cos(secondAngle);
-	var y = centerPos + secondHandRadius * Math.sin(secondAngle);
-	draw_hand(x,y,secondHandWidth, secondHandColor);
-}
-
-
 function drawCurrent() {
 	resize_to_window();
-	ctx.globalCompositeOperation = 'destination-over';
-	ctx.clearRect(0, 0, boxWidth, boxWidth); // clear canvas
 
-	draw_face();
+	clockFace.setSize(boxWidth);
+	clockFace.clearCtx();
 
 	var clock = new Clock();
 
-	var timeStr = clock.time.toString();
-	draw_time_string(timeStr);
+	clockFace.drawHands(clock);
 
-
-	draw_hands(clock);
+	clockFace.drawFace();
 
 	window.requestAnimationFrame(drawCurrent);
 }
@@ -128,7 +52,7 @@ function scale_constants() {
 	textRadius = centerPos * 0.75;
 	hourHandRadius = centerPos * 0.4;
 	minuteHandRadius = centerPos * 0.6;
-	secondHandRadius = centerPos * 0.65;
+	secondHandRadius = centerPos * 0.7;
 }
 
 function resize_to_window() {
@@ -154,6 +78,7 @@ function clock_init() {
 	canvas = document.getElementById('clock_widget');
 	ctx = canvas.getContext('2d');
 
+	clockFace = new ClockFace(ctx);
 
 	window.requestAnimationFrame(drawCurrent);
 }
